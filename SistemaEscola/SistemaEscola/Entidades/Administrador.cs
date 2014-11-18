9430a5f1b59add709f1f6d60420ab7ed542ceae1
Anderson Silva <andsf@live.com>
2014-11-17 23:54:10 -0200

    Adição das Entidades Alunos e Adiministrador, Checagem das Telas

---
+++ SistemaEscola/SistemaEscola/Entidades/Administrador.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEscola.Entidades
+{
+    class Administrador
+    {
+        String login;
+        int senha;
+
+        public String Login
+        {
+            get
+            {
+                return login;
+            }
+            set
+            {
+                login = value;
+            }
+        }
+
+        public int Senha
+        {
+            get
+            {
+                return senha;
+            }
+            set
+            {
+                senha = value;
+            }
+        }
+    }
+}
---
+++ SistemaEscola/SistemaEscola/Entidades/Aluno.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEscola.Entidades
+{
+    class Aluno
+    {
+        String  login, nome, senha, telefone, email, cpf;
+        DateTime nascimento;
+
+        public string Login
+        {
+            get
+            {
+                return login;
+            }
+            set
+            {
+                login = value;
+            }
+        }
+
+        public string Senha
+        {
+            get
+            {
+                return senha;
+            }
+            set
+            {
+                senha = value;
+            }
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public string Telefone
+        {
+            get
+            {
+                return telefone;
+            }
+            set
+            {
+                telefone = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+
+        public string CPF
+        {
+            get
+            {
+                return cpf;
+            }
+            set
+            {
+                cpf = value;
+            }
+        }
+
+        public DateTime Nascimento
+        {
+            get
+            {
+                return nascimento;
+            }
+            set
+            {
+                nascimento = value;
+            }
+        }
+    }
+}
