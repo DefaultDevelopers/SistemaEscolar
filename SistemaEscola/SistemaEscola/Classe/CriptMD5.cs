9181c9ef02db8d0285f05a9512d3e7f3a2dc5427
Anderson Silva <andsf@live.com>
2014-12-06 20:37:43 -0200

    Criptografia em MD5

---
+++ SistemaEscola/SistemaEscola/Classe/CriptMD5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEscola.Classe
+{
+    class CriptMD5
+    {
+        public string getMD5Hash(string cript)
+        {
+            System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(cript);
+            byte[] hash = md5.ComputeHash(inputBytes);
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
